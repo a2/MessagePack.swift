ccee1e910c2c434633a33f3aa7acc50bb0ef1bdb
Alexsander Akers <a2@pnd.mn>
2015-02-17 00:36:28 +0000

    Initial commit

---
+++ MessagePack/MessagePack.h
@@ -0,0 +1,19 @@
+//
+//  MessagePack.h
+//  MessagePack
+//
+//  Created by Alexsander Akers on 2/17/15.
+//  Copyright (c) 2015 Pandamonia LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessagePack.
+FOUNDATION_EXPORT double MessagePackVersionNumber;
+
+//! Project version string for MessagePack.
+FOUNDATION_EXPORT const unsigned char MessagePackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessagePack/PublicHeader.h>
+
+
